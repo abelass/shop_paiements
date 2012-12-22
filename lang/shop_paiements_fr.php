653559d0150e172fa238d1a8408e66c63b63ac01
abelass <websolutions@mychacra.net>
2012-12-22 10:35:07 +0100

    Configuration, les données du client et paiements

--- lang/shop_paiements_fr.php
+++ lang/shop_paiements_fr.php
@@ -5,18 +5,45 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// S
-	'shop_paiements_titre' => 'Shop Paiements',
+    //A
+    'atos'=>'Atos',
 
 	// C
 	'cash' => 'Cash',
 	"cash_explication"=>'Paiement cash<br/>Montant: @montant@ <br/>Vous recevrez un confimation de la commande par mail',	
-	'cfg_exemple' => 'Exemple',
-	'cfg_exemple_explication' => 'Explication de cet exemple',
 	'cfg_titre_parametrages' => 'Paramétrages',
 
+    //M
+    "message_paiement_cash"=>'Vous avez choisis d\'effectuer le paiement en cash',   
+    "message_paiement_cash"=>'Vous avez choisis d\'effectuer le paiement en cash',       
+    
+    //P
+    "paypal"=>'Paypal',     
+    
+    // S
+    'shop_paiements_titre' => 'Shop Paiements',      
+
 	// T
-	'titre_page_configurer_shop_paiements' => 'Shop Paiements',
+	'titre_adresse' => 'Adresse :',
+	'titre_bic' => 'BIC/SWIFT :',			
+	'titre_details_commercant' => 'Détails commercant :',	
+	'titre_iban' => 'IBAN :',	
+	'titre_paiements' => 'Les Paiements :',		
+	'titre_page_configurer_shop_paiements' => 'Shop Paiements :',
+	'titre_message_paiement_atos' => 'Le message à afficher pour le paiement par Atos :',
+	'titre_message_paiement_cash' => 'Le message à afficher pour le paiement en cash :',	
+	'titre_message_paiement_paypal' => 'Le message à afficher pour le paiement par Paypal :',
+	'titre_message_paiement_virement' => 'Le message à afficher pour le paiement par virement bancaire :',				
+	'titre_mobile' => 'Mobile :',		
+	'titre_nom_societe' => 'Nom de Société :',
+	'titre_nr_compte' => 'Compte bancaire :',		
+	'titre_nr_entreprise' => 'Numéro entreprise :',				
+	'titre_telephone' => 'Téléphone :',
+	'titre_tva' => 'Numéro TVA :',				
+	'titre_type_paiements' => 'Les types de paiements disponibles :',	
+	
+	//V
+	'virement'=>'Virement',
 );
 
-?>
+?>                         
