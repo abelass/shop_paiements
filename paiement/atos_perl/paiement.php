8287c5b73bda716dd95a5131c0e380631b3e4fd7
abelass <websolutions@mychacra.net>
2012-12-23 18:18:56 +0100

    adaptation de atos, basiquement fonctionnel, sans le retour, reste à l'intégrer plus proprement dans spip

--- paiement/atos_perl/paiement.php
+++ paiement/atos_perl/paiement.php
@@ -1,6 +1,6 @@
 <?php
 	//Charger SPIP
-	if (!defined('_ECRIRE_INC_VERSION')) {
+	/*if (!defined('_ECRIRE_INC_VERSION')) {
 		$currentdir = getcwd();
 		// recherche du loader SPIP.
 		$deep = 2;
@@ -15,33 +15,36 @@
 				require $lanceur;
 			}
 		}	
-	}
+	}*/
 	if (!defined('_ECRIRE_INC_VERSION')) {
 		die("<strong>Echec :</strong> SPIP ne peut pas etre demarre.<br />
 			Vous utilisez certainement un lien symbolique dans votre repertoire plugins.");
 	}	
-
+/*
     include_spip('inc/config');
+    $urlbase  = "http://domuni/";
+    $pathbase = "/home/abelass/www/domuni/";
     
 	// N� de Marchand
-	$merchant_id = lire_config('shop_paiements/atos_merchant_id');
-    $path_bin = "bin/request";	
+	$merchant_id = '014022286611111';
+    $pathbin = dirname(__FILE__).'/bin/';	
     $merchant_country= lire_config('shop_paiements/atos_merchant_country')?lire_config('shop_paiements/atos_merchant_country'):'fr';
-    $transaction_id=urlencode($_SESSION['ref']);
+    $total=1;
+    $transaction_id=urlencode('test');
     $currency_code=lire_config('shop_paiements/atos_currency_code')?lire_config('shop_paiements/atos_currency_code'):978;
-    $pathfile='conf/pathfile';
+    $pathfile = $pathbase._DIR_PLUGIN_SHOP_PAIEMENTS."paiement/atos_perl/conf/pathfile";
     $normal_return_url=lire_config('shop_paiements/atos_normal_return_url')?lire_config('shop_paiements/atos_normal_return_url'):'?page=transaction_merci';
     $cancel_return_url=lire_config('shop_paiements/atos_cancel_return_url')?lire_config('shop_paiements/atos_cancel_return_url'):'?page=transaction_regret';
     $auto_return_url='plugins/shop_paiements/atos_perl/paiement_atos_confirmation.php';
     $language=_request('lang')?_request('lang'):'fr';
     $urlbase =$GLOBALS['meta']['url_site'] ;
-    $urlperl = $urlbase . "conf/";
+    $urlperl = 'http://domuni/'._DIR_PLUGIN_SHOP_PAIEMENTS."paiement/atos_perl/conf";
     
 	session_start();
 	
 	$total = $_SESSION['total'];
 		
-	$total *= 100;
+	$total = 100;
 	
 	
 	print ("<HTML><HEAD><TITLE><:shop_paiements:titre_atos:></TITLE></HEAD>");
@@ -49,7 +52,7 @@
 	print ("<Font color=#000000>");
 	print ("<center><H1><:shop_paiements:atos_paiement_securise:></H1></center><br><br>");
 	print ("<center><H1>" . $GLOBALS['meta']['nom_site'] . "</H1></center><br><br>");
-
+*/
     /*SPIP
 	//		Affectation des param�tres obligatoires
 
@@ -122,7 +125,7 @@
 
 print ("</BODY></HTML>");*/
 
-
+/*
 $parm = '';
 $parm .= 'bindir='.$pathbin;
 $parm .= '&'.'merchant_id='.$merchant_id;
@@ -138,8 +141,12 @@ $parm .= '&'.'language='.$language;
 $parm .= '&'.'payment_means=CB,1,VISA,1,MASTERCARD,1';
 $parm .= '&'.'header_flag=no';
 
+echo serialize($parm);
+
 $result = file_get_contents($urlperl."/atos_request.pl?".$parm);
 
+
+echo serialize($result);
 list($rien, $code, $error, $message, $rien) = explode('!', $result);    
 
 if (($code == "") && ($error == "")) {
@@ -155,6 +162,81 @@ else {
     print ("<br><br>");
     print ("  $message <br>");
 }
-print ("</BODY></HTML>");
+print ("</BODY></HTML>");*/
 
+
+session_start();
+
+$total = _request('prix_total').'00';
+
+$transaction_id = _request('id_commande');
+
+$monsite = $GLOBALS['meta']['nom_site'];
+
+$urlbase  = "http://beta.domuni.eu/";
+$pathbase = "/home/www/bbc2856153279059177f97da97d2fbc1/web/beta/";
+
+// N° de Marchand
+$merchant_id = "042247252200016";
+
+    $auto_return_url   = $urlbase . find_in_path("paiement/atos_perl/confirmation.php");
+    $normal_return_url = $urlbase . "?fond=merci";
+    $cancel_return_url = $urlbase . "?fond=regret";
+
+    // Devise: 978 = Euro
+    $currency_code = 978;
+
+    // URL vers les scripts Perl (.pl), avec '/' final
+    $urlperl = $urlbase ._DIR_PLUGIN_SHOP_PAIEMENTS."paiement/atos_perl/conf/";
+
+    // Path ABSOLUs vers les binaires ATOS, et vers le path file
+    $pathbin = dirname(__FILE__)."/bin/";
+    $pathfile = $pathbase ._DIR_PLUGIN_SHOP_PAIEMENTS."paiement/atos_perl/conf/pathfile";
+
+    $language = 'fr';
+    $merchant_country = 'fr';
+
+
+print ("<HTML><HEAD><TITLE>ATOS - Paiement Securise sur Internet</TITLE></HEAD>");
+print ("<BODY bgcolor=#ffffff>");
+print ("<Font color=#000000>");
+print ("<center><H1>PAIEMENT SECURISE MERC@NET </H1></center><br><br>");
+print ("<center><H1>" . $monsite. "</H1></center><br><br>");
+
+// Version PERL
+$parm = '';
+$parm .= 'bindir='.$pathbin;
+$parm .= '&'.'merchant_id='.$merchant_id;
+$parm .= '&'.'merchant_country='.$merchant_country;
+$parm .= '&'.'amount='.$total;
+$parm .= '&'.'transaction_id='.$transaction_id;
+$parm .= '&'.'currency_code='.$currency_code;
+$parm .= '&'.'pathfile='.$pathfile;
+$parm .= '&'.'normal_return_url='.urlencode($normal_return_url);
+$parm .= '&'.'cancel_return_url='.urlencode($cancel_return_url);
+$parm .= '&'.'automatic_response_url='.urlencode($auto_return_url);
+$parm .= '&'.'language='.$language;
+$parm .= '&'.'payment_means=CB,1,VISA,1,MASTERCARD,1';
+$parm .= '&'.'header_flag=no';
+
+
+$result = file_get_contents($urlperl."atos_request.pl?".$parm);
+
+list($rien, $code, $error, $message, $rien) = explode('!', $result);
+
+
+if (($code == "") && ($error == "")) {
+    print ("<BR><CENTER>erreur appel request</CENTER><BR>");
+    print ("executable request non trouve $pathbin");
+}
+else if ($code != 0) {
+    print ("<center><b><h2>Erreur appel API de paiement.</h2></center></b>");
+    print ("<br><br><br>");
+    print ("Message erreur : $error <br>");
+}
+else {
+    print ("<br><br>");
+    print ("  $message <br>");
+}
+print ("</BODY></HTML>");
 ?>
