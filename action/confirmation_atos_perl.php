27c44ba3b0c4b02855e01bef46957ce167ea2f32
abelass <websolutions@mychacra.net>
2013-01-07 20:27:21 +0100

    faire fonctionner le retour, encore un problème avec l'executable '"executable response non trouve "

---
+++ action/confirmation_atos_perl.php
@@ -0,0 +1,171 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+spip_log("paiement retour 1",'shop_paiements');
+
+
+function action_confirmation_atos_perl_dist($arg=null) {
+    spip_log("paiement retour 2",'shop_paiements');
+    include_spip('base/abstract_sql');
+    include_spip('inc/config');
+    $urlbase  = $GLOBALS['meta']['adresse_site'].'/';
+
+    //Reduire la référence comme atos ene permmet euelment 6 characters pour l'id
+
+    
+    // N° de Marchand
+    $merchant_id = lire_config('shop_paiements/merchant_id_atos_perl');
+    
+    $pathbase =  str_replace(_DIR_PLUGIN_SHOP_PAIEMENTS.'formulaires','',dirname(__FILE__));
+
+   //Calculer le prix
+   $total=array_sum($montants).'00';
+
+
+
+    // URL vers les scripts Perl (.pl), avec '/' final
+    $urlperl = $urlbase ._DIR_PLUGIN_SHOP_PAIEMENTS."paiement/atos_perl/conf/";
+
+    // Path ABSOLUs vers les binaires ATOS, et vers le path file
+    $pathbin = $pathbase.'/'._DIR_PLUGIN_SHOP_PAIEMENTS."paiement/atos_perl/bin/";
+    $pathfile = $pathbase ._DIR_PLUGIN_SHOP_PAIEMENTS."paiement/atos_perl/conf/pathfile";
+
+
+$data= escapeshellcmd($_POST["DATA"]);
+
+ spip_log($data,'shop_paiements');
+ spip_log($pathbin,'shop_paiements');
+
+
+spip_log("paiement retour params ",'shop_paiements');
+
+// Appel du binaire response
+$result = file_get_contents($urlperl."atos_response.pl?pathfile=$pathfile&message=$data&bindir=$pathbin");
+
+$tableau = explode ("!", $result);
+
+spip_log($tableau,'shop_paiements');
+
+$code = $tableau[1];
+$error = $tableau[2];
+$merchant_id = $tableau[3];
+$merchant_country = $tableau[4];
+$amount = $tableau[5];
+$transaction_id = $tableau[6];
+$payment_means = $tableau[7];
+$transmission_date= $tableau[8];
+$payment_time = $tableau[9];
+$payment_date = $tableau[10];
+$response_code = $tableau[11];
+$payment_certificate = $tableau[12];
+$authorisation_id = $tableau[13];
+$currency_code = $tableau[14];
+$card_number = $tableau[15];
+$cvv_flag = $tableau[16];
+$cvv_response_code = $tableau[17];
+$bank_response_code = $tableau[18];
+$complementary_code = $tableau[19];
+$return_context = $tableau[20];
+$caddie = $tableau[21];
+$receipt_complement = $tableau[22];
+$merchant_language = $tableau[23];
+$language = $tableau[24];
+$customer_id = $tableau[25];
+$order_id = $tableau[26];
+$customer_email = $tableau[27];
+$customer_ip_address = $tableau[28];
+$capture_day = $tableau[29];
+$capture_mode = $tableau[30];
+$data = $tableau[31];
+
+$valeurs=array('type_payement'=>'atos');
+
+
+  //on institue la commande
+  if($action = charger_fonction('instituer_commande', 'action',true)) {
+
+      switch($response_code){
+   case '00':
+      /* Transaction approved */
+      $action($id_commande."-paye");
+       break;
+   case '12':
+      /* Invalid amount
+       PROBLEM LOCATION: MERCHANT */
+      $action($id_commande."-partiel");
+      break;
+      
+      
+  } 
+
+
+    
+}
+     sql_updateq('spip_commandes', $valeurs, 'id_commande=' . sql_quote($transaction_id));
+
+
+$logfile="/var/log/atos/site";
+
+    // Ouverture du fichier de log en append
+
+    $fp=fopen($logfile, "a");
+    //  analyse du code retour
+fputs($fp, "code $customer_ip_address \n");
+
+  if (( $code == "" ) && ( $error == "" ) )
+    {
+    fwrite($fp, "erreur appel response\n");
+    print ("executable response non trouve $path_bin\n");
+    spip_log("erreur appel response\n",'shop_paiements');
+    spip_log("executable response non trouve $path_bin\n",'shop_paiements');    
+    }
+
+    //  Erreur, sauvegarde le message d'erreur
+
+    else if ( $code != 0 ){
+        fwrite($fp, " API call error.\n");
+        fwrite($fp, "Error message :  $error\n");
+        spip_log(" API call error.\n",'shop_paiements');
+        spip_log( "Error message :  $error\n",'shop_paiements');   
+    }
+    else {
+
+    // OK, Sauvegarde des champs de la r�ponse
+
+    fwrite( $fp, "merchant_id : $merchant_id\n");
+    fwrite( $fp, "merchant_country : $merchant_country\n");
+    fwrite( $fp, "amount : $amount\n");
+    fwrite( $fp, "transaction_id : $transaction_id\n");
+    fwrite( $fp, "transmission_date: $transmission_date\n");
+    fwrite( $fp, "payment_means: $payment_means\n");
+    fwrite( $fp, "payment_time : $payment_time\n");
+    fwrite( $fp, "payment_date : $payment_date\n");
+    fwrite( $fp, "response_code : $response_code\n");
+    fwrite( $fp, "payment_certificate : $payment_certificate\n");
+    fwrite( $fp, "authorisation_id : $authorisation_id\n");
+    fwrite( $fp, "currency_code : $currency_code\n");
+    fwrite( $fp, "card_number : $card_number\n");
+    fwrite( $fp, "cvv_flag: $cvv_flag\n");
+    fwrite( $fp, "cvv_response_code: $cvv_response_code\n");
+    fwrite( $fp, "bank_response_code: $bank_response_code\n");
+    fwrite( $fp, "complementary_code: $complementary_code\n");
+    fwrite( $fp, "return_context: $return_context\n");
+    fwrite( $fp, "caddie : $caddie\n");
+    fwrite( $fp, "receipt_complement: $receipt_complement\n");
+    fwrite( $fp, "merchant_language: $merchant_language\n");
+    fwrite( $fp, "language: $language\n");
+    fwrite( $fp, "customer_id: $customer_id\n");
+    fwrite( $fp, "order_id: $order_id\n");
+    fwrite( $fp, "customer_email: $customer_email\n");
+    fwrite( $fp, "customer_ip_address: $customer_ip_address\n");
+    fwrite( $fp, "capture_day: $capture_day\n");
+    fwrite( $fp, "capture_mode: $capture_mode\n");
+    fwrite( $fp, "data: $data\n");
+    fwrite( $fp, "-------------------------------------------\n");
+    spip_log(" paiement ok\n",'shop_paiements');
+    }
+
+    fclose ($fp);
+  
+}
+?>
--- formulaires/atos_perl.php
+++ formulaires/atos_perl.php
@@ -1,187 +1,46 @@
 <?php
-	//Charger SPIP
-	/*if (!defined('_ECRIRE_INC_VERSION')) {
-		$currentdir = getcwd();
-		// recherche du loader SPIP.
-		$deep = 2;
-		$lanceur ='ecrire/inc_version.php';
-		$include = '../../'.$lanceur;
-		while (!defined('_ECRIRE_INC_VERSION') && $deep++ < 6) { 
-			// attention a pas descendre trop loin tout de meme ! 
-			// plugins/zone/stable/nom/version/tests/ maximum cherche
-			$include = '../' . $include;
-			if (file_exists($include)) {
-				chdir(dirname(dirname($include)));
-				require $lanceur;
-			}
-		}	
-	}*/
-	if (!defined('_ECRIRE_INC_VERSION')) {
-		die("<strong>Echec :</strong> SPIP ne peut pas etre demarre.<br />
-			Vous utilisez certainement un lien symbolique dans votre repertoire plugins.");
-	}	
-/*
+
+// Sécurité
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function formulaires_atos_perl_charger_dist($options = array()){
     include_spip('inc/config');
-    $urlbase  = "http://domuni/";
-    $pathbase = "/home/abelass/www/domuni/";
     
-	// N� de Marchand
-	$merchant_id = '014022286611111';
-    $pathbin = dirname(__FILE__).'/bin/';	
-    $merchant_country= lire_config('shop_paiements/atos_merchant_country')?lire_config('shop_paiements/atos_merchant_country'):'fr';
-    $total=1;
-    $transaction_id=urlencode('test');
-    $currency_code=lire_config('shop_paiements/atos_currency_code')?lire_config('shop_paiements/atos_currency_code'):978;
-    $pathfile = $pathbase._DIR_PLUGIN_SHOP_PAIEMENTS."paiement/atos_perl/conf/pathfile";
-    $normal_return_url=lire_config('shop_paiements/atos_normal_return_url')?lire_config('shop_paiements/atos_normal_return_url'):'?page=transaction_merci';
-    $cancel_return_url=lire_config('shop_paiements/atos_cancel_return_url')?lire_config('shop_paiements/atos_cancel_return_url'):'?page=transaction_regret';
-    $auto_return_url='plugins/shop_paiements/atos_perl/paiement_atos_confirmation.php';
-    $language=_request('lang')?_request('lang'):'fr';
-    $urlbase =$GLOBALS['meta']['url_site'] ;
-    $urlperl = 'http://domuni/'._DIR_PLUGIN_SHOP_PAIEMENTS."paiement/atos_perl/conf";
+    spip_log("paiment atos charger",'shop_paiements');
+
+    session_start();
     
-	session_start();
-	
-	$total = $_SESSION['total'];
-		
-	$total = 100;
-	
-	
-	print ("<HTML><HEAD><TITLE><:shop_paiements:titre_atos:></TITLE></HEAD>");
-	print ("<BODY bgcolor=#ffffff>");
-	print ("<Font color=#000000>");
-	print ("<center><H1><:shop_paiements:atos_paiement_securise:></H1></center><br><br>");
-	print ("<center><H1>" . $GLOBALS['meta']['nom_site'] . "</H1></center><br><br>");
-*/
-    /*SPIP
-	//		Affectation des param�tres obligatoires
+    $valeurs = $options;
+    $details=$options['details'];
+    //Reduire la référence comme atos ene permmet euelment 6 characters pour l'id
+    $transaction_id = substr($options['transaction_id'], 0, 6);
+    $monsite = $GLOBALS['meta']['nom_site'];
+    $urlbase  = $GLOBALS['meta']['adresse_site'].'/';
+    
+    // N° de Marchand
+    $merchant_id = lire_config('shop_paiements/merchant_id_atos_perl');
+    
+    $language = _request('lang');
+    $merchant_country = strtolower(sql_getfetsel('code','spip_pays','id_pays='.lire_config('shop_paiements/merchant_country_atos_perl')));
 
-	$parm="merchant_id=$merchant_id";
-	$parm="$parm merchant_country=fr";
-	$parm="$parm amount=$total";
-	$parm="$parm currency_code=978";
+    $montants=array();
+    if(is_array($details)){
+          foreach($details AS $detail){
+              $montants[]=$detail['quantite']*$detail['prix'];
+          }
+      }
+    
+    
 
+    $pathbase =  str_replace(_DIR_PLUGIN_SHOP_PAIEMENTS.'formulaires','',dirname(__FILE__));
 
-	// Initialisation du chemin du fichier pathfile (� modifier)
-    //   ex :
-    //    -> Windows : $parm="$parm pathfile=c:\\repertoire\\pathfile";
-    //    -> Unix    : $parm="$parm pathfile=/home/repertoire/pathfile";
-    //
-    // Cette variable est facultative. Si elle n'est pas renseign�e,
-    // l'API positionne la valeur � "./pathfile".
-
-		$parm="$parm pathfile=conf/pathfile";
-
-	//		Si aucun transaction_id n'est affect�, request en g�n�re
-	//		un automatiquement � partir de heure/minutes/secondes
-	//		R�f�rez vous au Guide du Programmeur pour
-	//		les r�serves �mises sur cette fonctionnalit�
-	//
-	
-	#$parm="$parm transaction_id=" . urlencode($_SESSION['ref']);
-	
-	$path_bin = "bin/request";
+   //Calculer le prix
+   $total=array_sum($montants).'00';
 
 
-	//	Appel du binaire request
-	chdir($currentdir); // Il faut revenir dans le dossier du script de paiement pour trouver les binaires !!!!
-	$result=exec("$path_bin $parm");
-
-	//	sortie de la fonction : $result=!code!error!buffer!
-	//	    - code=0	: la fonction g�n�re une page html contenue dans la variable buffer
-	//	    - code=-1 	: La fonction retourne un message d'erreur dans la variable error
-
-	//On separe les differents champs et on les met dans une variable tableau
-
-	$tableau = explode ("!", "$result");
-
-	//	r�cup�ration des param�tres
-
-	$code = $tableau[1];
-	$error = $tableau[2];
-	$message = $tableau[3];
-
-	//  analyse du code retour
-
-  if (( $code == "" ) && ( $error == "" ) )
- 	{
-  	print ("<BR><CENTER>erreur appel request</CENTER><BR>");
-  	print ("executable request non trouve $path_bin");
- 	}
-
-	//	Erreur, affiche le message d'erreur
-
-	else if ($code != 0){
-		print ("<center><b><h2>Erreur appel API de paiement.</h2></center></b>");
-		print ("<br><br><br>");
-		print (" message erreur : $error <br>");
-	}
-
-	//	OK, affiche le formulaire HTML
-	else {
-		print ("<br><br>");
-		print ("  $message <br>");
-	}
-
-print ("</BODY></HTML>");*/
-
-/*
-$parm = '';
-$parm .= 'bindir='.$pathbin;
-$parm .= '&'.'merchant_id='.$merchant_id;
-$parm .= '&'.'merchant_country='.$merchant_country;
-$parm .= '&'.'amount='.$total;
-$parm .= '&'.'transaction_id='.$transaction_id;
-$parm .= '&'.'currency_code='.$currency_code;
-$parm .= '&'.'pathfile='.$pathfile;
-$parm .= '&'.'normal_return_url='.urlencode($normal_return_url);
-$parm .= '&'.'cancel_return_url='.urlencode($cancel_return_url);
-$parm .= '&'.'automatic_response_url='.urlencode($auto_return_url);
-$parm .= '&'.'language='.$language;
-$parm .= '&'.'payment_means=CB,1,VISA,1,MASTERCARD,1';
-$parm .= '&'.'header_flag=no';
-
-echo serialize($parm);
-
-$result = file_get_contents($urlperl."/atos_request.pl?".$parm);
-
-
-echo serialize($result);
-list($rien, $code, $error, $message, $rien) = explode('!', $result);    
-
-if (($code == "") && ($error == "")) {
-    print ("<BR><CENTER>erreur appel request</CENTER><BR>");
-    print ("executable request non trouve $pathbin");
-}
-else if ($code != 0) {
-    print ("<center><b><h2>Erreur appel API de paiement.</h2></center></b>");
-    print ("<br><br><br>");
-    print ("Message erreur : $error <br>");
-}
-else {
-    print ("<br><br>");
-    print ("  $message <br>");
-}
-print ("</BODY></HTML>");*/
-
-
-session_start();
-
-$total = _request('prix_total').'00';
-
-$transaction_id = _request('id_commande');
-
-$monsite = $GLOBALS['meta']['nom_site'];
-
-$urlbase  = "http://beta.domuni.eu/";
-$pathbase = "/home/www/bbc2856153279059177f97da97d2fbc1/web/beta/";
-
-// N° de Marchand
-$merchant_id = "042247252200016";
-
-    $auto_return_url   = $urlbase . find_in_path("paiement/atos_perl/confirmation.php");
-    $normal_return_url = $urlbase . "?fond=merci";
-    $cancel_return_url = $urlbase . "?fond=regret";
+    $auto_return_url   = $urlbase . 'spip.php?action=confirmation_atos_perl';
+    $normal_return_url = $urlbase . "spip.php?page=transaction_merci";
+    $cancel_return_url = $urlbase . "spip.php?page=transaction_regret";
 
     // Devise: 978 = Euro
     $currency_code = 978;
@@ -190,18 +49,9 @@ $merchant_id = "042247252200016";
     $urlperl = $urlbase ._DIR_PLUGIN_SHOP_PAIEMENTS."paiement/atos_perl/conf/";
 
     // Path ABSOLUs vers les binaires ATOS, et vers le path file
-    $pathbin = dirname(__FILE__)."/bin/";
+    $pathbin = $pathbase.'/'._DIR_PLUGIN_SHOP_PAIEMENTS."paiement/atos_perl/bin/";
     $pathfile = $pathbase ._DIR_PLUGIN_SHOP_PAIEMENTS."paiement/atos_perl/conf/pathfile";
-
-    $language = 'fr';
-    $merchant_country = 'fr';
-
-
-print ("<HTML><HEAD><TITLE>ATOS - Paiement Securise sur Internet</TITLE></HEAD>");
-print ("<BODY bgcolor=#ffffff>");
-print ("<Font color=#000000>");
-print ("<center><H1>PAIEMENT SECURISE MERC@NET </H1></center><br><br>");
-print ("<center><H1>" . $monsite. "</H1></center><br><br>");
+    //$pathfile = find_in_path("paiement/atos_perl/conf/pathfile");
 
 // Version PERL
 $parm = '';
@@ -219,8 +69,10 @@ $parm .= '&'.'language='.$language;
 $parm .= '&'.'payment_means=CB,1,VISA,1,MASTERCARD,1';
 $parm .= '&'.'header_flag=no';
 
+    echo $parm;
 
 $result = file_get_contents($urlperl."atos_request.pl?".$parm);
+
 
 list($rien, $code, $error, $message, $rien) = explode('!', $result);
 
@@ -236,7 +88,11 @@ else if ($code != 0) {
 }
 else {
     print ("<br><br>");
-    print ("  $message <br>");
+    print ('<div class="logos_atos">'.$message.'</div>');
 }
-print ("</BODY></HTML>");
+
+
+return $valeurs;
+}
+
 ?>
