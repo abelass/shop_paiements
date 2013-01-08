99289b34280eecd355a9c84fa43c7bc3f548b023
abelass <websolutions@mychacra.net>
2013-01-08 11:18:16 +0100

    vider le panier après le retour

--- action/confirmation_atos_perl.php
+++ action/confirmation_atos_perl.php
@@ -31,54 +31,50 @@ function action_confirmation_atos_perl_dist($arg=null) {
     $pathfile = $pathbase ._DIR_PLUGIN_SHOP_PAIEMENTS."paiement/atos_perl/conf/pathfile";
 
 
-$data= escapeshellcmd($_POST["DATA"]);
+    $data= escapeshellcmd($_POST["DATA"]);
+    
+    spip_log($data,'shop_paiements');
 
- spip_log($data,'shop_paiements');
- spip_log($pathbin,'shop_paiements');
-
-
-spip_log("paiement retour params ",'shop_paiements');
-
-// Appel du binaire response
-$result = file_get_contents($urlperl."atos_response.pl?pathfile=$pathfile&message=$data&bindir=$pathbin");
-
-$tableau = explode ("!", $result);
-
-spip_log($tableau,'shop_paiements');
-
-$code = $tableau[1];
-$error = $tableau[2];
-$merchant_id = $tableau[3];
-$merchant_country = $tableau[4];
-$amount = $tableau[5];
-$transaction_id = $tableau[6];
-$payment_means = $tableau[7];
-$transmission_date= $tableau[8];
-$payment_time = $tableau[9];
-$payment_date = $tableau[10];
-$response_code = $tableau[11];
-$payment_certificate = $tableau[12];
-$authorisation_id = $tableau[13];
-$currency_code = $tableau[14];
-$card_number = $tableau[15];
-$cvv_flag = $tableau[16];
-$cvv_response_code = $tableau[17];
-$bank_response_code = $tableau[18];
-$complementary_code = $tableau[19];
-$return_context = $tableau[20];
-$caddie = $tableau[21];
-$receipt_complement = $tableau[22];
-$merchant_language = $tableau[23];
-$language = $tableau[24];
-$customer_id = $tableau[25];
-$order_id = $tableau[26];
-$customer_email = $tableau[27];
-$customer_ip_address = $tableau[28];
-$capture_day = $tableau[29];
-$capture_mode = $tableau[30];
-$data = $tableau[31];
-
-$valeurs=array('type_paiement'=>'atos');
+    // Appel du binaire response
+    $result = file_get_contents($urlperl."atos_response.pl?pathfile=$pathfile&message=$data&bindir=$pathbin");
+    
+    $tableau = explode ("!", $result);
+    
+    spip_log($tableau,'shop_paiements');
+    
+    $code = $tableau[1];
+    $error = $tableau[2];
+    $merchant_id = $tableau[3];
+    $merchant_country = $tableau[4];
+    $amount = $tableau[5];
+    $transaction_id = $tableau[6];
+    $payment_means = $tableau[7];
+    $transmission_date= $tableau[8];
+    $payment_time = $tableau[9];
+    $payment_date = $tableau[10];
+    $response_code = $tableau[11];
+    $payment_certificate = $tableau[12];
+    $authorisation_id = $tableau[13];
+    $currency_code = $tableau[14];
+    $card_number = $tableau[15];
+    $cvv_flag = $tableau[16];
+    $cvv_response_code = $tableau[17];
+    $bank_response_code = $tableau[18];
+    $complementary_code = $tableau[19];
+    $return_context = $tableau[20];
+    $caddie = $tableau[21];
+    $receipt_complement = $tableau[22];
+    $merchant_language = $tableau[23];
+    $language = $tableau[24];
+    $customer_id = $tableau[25];
+    $order_id = $tableau[26];
+    $customer_email = $tableau[27];
+    $customer_ip_address = $tableau[28];
+    $capture_day = $tableau[29];
+    $capture_mode = $tableau[30];
+    $data = $tableau[31];
+    
+    $valeurs=array('type_paiement'=>'atos');
 
 
   //on institue la commande
@@ -103,71 +99,18 @@ $valeurs=array('type_paiement'=>'atos');
 
     
 }
+     //Insérer le type de paiement
      sql_updateq('spip_commandes', $valeurs, 'id_commande=' . sql_quote($transaction_id));
+     
+    //Éliminer le panier lié à la commande
+    $reference = $flux['args']['paypal']['invoice'];
+    $commande = sql_fetsel('id_commande, statut, id_auteur', 'spip_commandes', 'id_commande = '.sql_quote($transaction_id));
+    $objet=sql_fetsel('objet,id_objet','spip_commandes_details','id_commande='.$commande['id_commande']);
+    $id_panier=sql_getfetsel('id_panier','spip_paniers_liens','id_objet='.$objet['id_objet'].' AND objet='.sql_quote($objet['objet']));
+    sql_delete('spip_paniers_liens','id_panier='.$id_panier);
+    sql_delete('spip_paniers','id_panier='.$id_panier);
+    spip_log("Retour atos_perl eliminer panier $id_panier",'shopaiements' . _LOG_INFO);
 
-
-$logfile="/var/log/atos/site";
-
-    // Ouverture du fichier de log en append
-
-    $fp=fopen($logfile, "a");
-    //  analyse du code retour
-fputs($fp, "code $customer_ip_address \n");
-
-  if (( $code == "" ) && ( $error == "" ) )
-    {
-    fwrite($fp, "erreur appel response\n");
-    print ("executable response non trouve $path_bin\n");
-    spip_log("erreur appel response\n",'shop_paiements');
-    spip_log("executable response non trouve $path_bin\n",'shop_paiements');    
-    }
-
-    //  Erreur, sauvegarde le message d'erreur
-
-    else if ( $code != 0 ){
-        fwrite($fp, " API call error.\n");
-        fwrite($fp, "Error message :  $error\n");
-        spip_log(" API call error.\n",'shop_paiements');
-        spip_log( "Error message :  $error\n",'shop_paiements');   
-    }
-    else {
-
-    // OK, Sauvegarde des champs de la r�ponse
-
-    fwrite( $fp, "merchant_id : $merchant_id\n");
-    fwrite( $fp, "merchant_country : $merchant_country\n");
-    fwrite( $fp, "amount : $amount\n");
-    fwrite( $fp, "transaction_id : $transaction_id\n");
-    fwrite( $fp, "transmission_date: $transmission_date\n");
-    fwrite( $fp, "payment_means: $payment_means\n");
-    fwrite( $fp, "payment_time : $payment_time\n");
-    fwrite( $fp, "payment_date : $payment_date\n");
-    fwrite( $fp, "response_code : $response_code\n");
-    fwrite( $fp, "payment_certificate : $payment_certificate\n");
-    fwrite( $fp, "authorisation_id : $authorisation_id\n");
-    fwrite( $fp, "currency_code : $currency_code\n");
-    fwrite( $fp, "card_number : $card_number\n");
-    fwrite( $fp, "cvv_flag: $cvv_flag\n");
-    fwrite( $fp, "cvv_response_code: $cvv_response_code\n");
-    fwrite( $fp, "bank_response_code: $bank_response_code\n");
-    fwrite( $fp, "complementary_code: $complementary_code\n");
-    fwrite( $fp, "return_context: $return_context\n");
-    fwrite( $fp, "caddie : $caddie\n");
-    fwrite( $fp, "receipt_complement: $receipt_complement\n");
-    fwrite( $fp, "merchant_language: $merchant_language\n");
-    fwrite( $fp, "language: $language\n");
-    fwrite( $fp, "customer_id: $customer_id\n");
-    fwrite( $fp, "order_id: $order_id\n");
-    fwrite( $fp, "customer_email: $customer_email\n");
-    fwrite( $fp, "customer_ip_address: $customer_ip_address\n");
-    fwrite( $fp, "capture_day: $capture_day\n");
-    fwrite( $fp, "capture_mode: $capture_mode\n");
-    fwrite( $fp, "data: $data\n");
-    fwrite( $fp, "-------------------------------------------\n");
-    spip_log(" paiement ok\n",'shop_paiements');
-    }
-
-    fclose ($fp);
   
 }
 ?>
