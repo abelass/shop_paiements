3935b406f9b6e226c11ffef5f63ef3f2101d8847
abelass <websolutions@mychacra.net>
2013-01-08 12:37:09 +0100

    plus besoin

--- paiement/atos_perl/confirmation.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Sécurité
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
-    spip_log("paiement retour confirmation.php",'shop_paiements');
-
-    $action=charger_fonction('confirmation_atos_perl','action/');
-    $panier();
-?>
